a78854df71ae5b2d4477a5e3966667ff1ddd3636
Ujjwal Maheshwari <ujjwal2327@gmail.com>
2026-06-21 08:59:35 +0530

    fix deploy bugs

---
+++ types/utif.d.ts
@@ -0,0 +1,12 @@
+declare module 'utif' {
+  export interface IFD {
+    width: number
+    height: number
+    [key: string]: unknown
+  }
+
+  export function decode(buffer: ArrayBuffer | Uint8Array): IFD[]
+  export function decodeImage(buffer: ArrayBuffer | Uint8Array, ifd: IFD, ifds?: IFD[]): void
+  export function toRGBA8(ifd: IFD): Uint8Array
+  export function encodeImage(rgba: Uint8Array, width: number, height: number): ArrayBuffer
+}
